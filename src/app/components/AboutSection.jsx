0456d6251081ddee1c17fab7cde1686b40959f88
maksof-amanullah <120739260+maksof-aman@users.noreply.github.com>
2024-08-28 10:32:07 +0500

    fix

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,78 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Next.js</li>
+        <li>PostgreSQL</li>
+        <li>Angular</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Inter from Goverment College </li>
+        <li>Web Development ourse from Saylani Mass IT Training </li>
+        <li>Virtual University of Pakistan</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+          With a strong passion for both technology and creativity, I specialize in web development and graphic design. I bring ideas to life by crafting visually appealing websites that are not only functional but also engaging. My experience in designing user-friendly interfaces, combined with my technical skills, allows me to create seamless digital experiences that leave a lasting impact. Whether it's building a responsive website or designing a compelling visual brand, I am dedicated to delivering high-quality work that meets the needs of my clients.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
